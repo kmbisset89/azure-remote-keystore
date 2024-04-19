750676ce13f2dc8d9185a7d98d8cf438b67498ed
Kerry Bisset <kerry.bisset@i3-corps.com>
2024-04-19 18:25:20 -0500

    Updated Java Version

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "17"
+                jvmTarget = "11"
             }
         }
     }
@@ -62,8 +62,8 @@ android {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
